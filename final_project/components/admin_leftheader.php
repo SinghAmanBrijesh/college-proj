742177d3f971a03e388af809c28db62ded7b1448
SinghAmanBrijesh <118956523+SinghAmanBrijesh@users.noreply.github.com>
2023-05-02 13:56:03 +0530

    Add files via upload

---
+++ final_project/components/admin_leftheader.php
@@ -0,0 +1,42 @@
+<div class="navcontainer">
+            <nav class="nav">
+                <div class="nav-upper-options">
+                    <div class="nav-option option1">
+
+                        <h3> Dashboard</h3>
+                    </div>
+
+                    <div class="option2 nav-option">
+
+                        <h3> <a href="dashboard.php">Home</a></h3>
+                    </div>
+
+                    <div class="nav-option option3">
+
+                        <h3><a href="products.php">Products</a></h3>
+                    </div>
+
+                    <div class="nav-option option4">
+
+                        <h3> <a href="placed_orders.php">Orders</a></h3>
+                    </div>
+
+                    <div class="nav-option option5">
+
+                        <h3> <a href="admin_accounts.php">Admins</a></h3>
+                    </div>
+
+                    <div class="nav-option option6">
+
+                        <h3> <a href="users_accounts.php">Users</a></h3>
+                    </div>
+
+
+                    <div class="nav-option option7">
+
+                        <h3> <a href="messages.php">Messages</a></h3>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
